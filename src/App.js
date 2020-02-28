ee8488d8747b3f503d7291650445729ebdf38723
Tiffany Crosby <tiffany.crosby65@gmail.com>
2020-02-28 17:49:39 -0500

    changed apps

--- src/App.js
+++ src/App.js
@@ -3,17 +3,17 @@ import Header from "./components/Header.js";
 import { Route } from "react-router-dom";
 import WelcomePage from "./components/WelcomePage";
 import CharacterList from "./components/CharacterList";
-import LocationList from "./components/LocationsList";
+import LocationsList from "./components/LocationsList";
 import EpisodeList from "./components/EpisodesList";
 
 export default function App() {
   return (
     <main>
       <Header />
-      <Route exact path="/" render={props => { return <WelcomePage {...props} />; }}></Route>
-      <Route path="/characters" render={props => { return <CharacterList {...props} />; }}></Route>
-      <Route path="/locations" render={props => { return <LocationList {...props} />; }}></Route>
-      <Route path="/episodes" render={props => { return <EpisodeList {...props} />; }}></Route>
-    </main>
+      <Route exact path="/" component={WelcomePage}><WelcomePage /></Route >
+      <Route path="/characters" component={CharacterList}><CharacterList /></Route>
+      <Route path="/locations" component={LocationsList}><LocationsList /></Route>
+      <Route path="/episodes" component={EpisodeList}><EpisodeList /></Route>
+    </main >
   );
 }
